8b004c28fd25ac5eb9f7f847cb02e864f500a4b8
Thorsten Fuchs <thorsten2991@gmx.de>
2015-12-09 20:42:31 +0100

    Exercise 1 implemented, does not yet produce files for plotting

--- ex2/Source/cacheline.c
+++ ex2/Source/cacheline.c
@@ -1,17 +1,58 @@
 #include <stdio.h>
-#include <sys/time.h>
+#include <stdlib.h>
+#include <time.h>
 
-static inline
-double gettime(void) {
-	struct timeval time;
+const int n = 4096 * 1024;
+const int stride = 1024;
 
-	gettimeofday(&time, NULL);
-	return time.tv_sec + time.tv_usec / 1000000.0;
+//  Allocate memory of size size
+static 
+int* ivalloc(int value, size_t num, size_t size) { 
+	int* ret = (int*)malloc(num*size);
+	
+	for(int i = num; i > 0;)
+	{
+		ret[--i] = value;
+	}
+
+	return ret;
 }
 
-int main(int argc, char* argv[])
+static inline
+double gettimediff(struct timespec end, struct timespec start) {
+	double ret = end.tv_sec + (double)end.tv_nsec/1000000000.0 - start.tv_sec - (double)start.tv_nsec/1000000000.0;
+
+	return ret;
+}
+
+
+int main(int argc, char* argv[]) 
 {
-	// TODO: Implementation
+	int* array = (int*)ivalloc(1, n, sizeof(int));
+
+	int dummy = 0;
+
+	struct timespec start;
+	struct timespec end;
+
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
+
+	for(int i = n-1; i >= 0; i -= stride) 
+	{
+		dummy += array[i];
+	} 
+
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+
+	double t = gettimediff(end, start);
+
+	printf("Values read: %d\n", dummy);
+
+	printf("Time per element: %2.9f\n", t / dummy);
+
+	printf("Total time: %2.9f\n", t); 
+
+	free(array);
 	
 	return 0;
 }
