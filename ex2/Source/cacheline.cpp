eb7da3999a02e517e4d49f353d571bfa6ac7e9ed
Jonas Sticha <jonas.sticha@tum.de>
2015-12-14 11:33:11 +0100

    Improve time measurement

--- ex2/Source/cacheline.cpp
+++ ex2/Source/cacheline.cpp
@@ -1,20 +1,30 @@
 #include <stdio.h>
-#include <sys/time.h>
+#include <time.h>
 #include <iostream>
 
 using namespace std;
 
-const int N = 1024*4096;
-const int stride = 1;
+const int N = 4096*4096;
+clockid_t clockId = CLOCK_MONOTONIC_RAW;
 
 static inline double getMilliSeconds(void) {
-	struct timeval time;
-	gettimeofday(&time, NULL);
-	return (time.tv_sec * 1000) + (time.tv_usec / 1000.0);
+	struct timespec time;
+	clock_gettime(clockId, &time);
+	return (time.tv_sec * 1000) + (time.tv_nsec / 1000000.0);
+}
+
+static inline double getMicroSeconds(void) {
+	struct timespec time;
+	clock_gettime(clockId, &time);
+	return (time.tv_sec * 1000000) + (time.tv_nsec / 1000.0);
 }
 
 char* initMemory() {
-	return (char*) malloc(N * sizeof(char));
+	char* mem = (char*) malloc(N * sizeof(char));
+	for(int i = 0; i < N; i++) {
+		mem[i] = 1;
+	}
+	return mem;
 }
 
 inline unsigned int stridedSum(char* mem, int stride) {
@@ -22,16 +32,18 @@ inline unsigned int stridedSum(char* mem, int stride) {
 	for(int i = 0; i < N-stride; i += stride){
 		sum += mem[i];
 	}
+	return sum;
 }
 
-void doMemAccessMeasuremnts(char* mem, int stride) {
-	cout << "Starting memory access performance measurement..." << endl;
+void doMemAccessMeasurements(char *mem, int stride) {
+	cout << "-----------------------------------------------------------------" << endl;
 	cout << "Stride: " << stride << endl;
-	double start = getMilliSeconds();
-	stridedSum(mem, stride);
-	double end = getMilliSeconds();
-	double totalMillis = end - start;
-	cout << "Total time of execution: " << totalMillis << " milliseconds." << endl;
+	double start = getMicroSeconds();
+	int sum = stridedSum(mem, stride);
+	double end = getMicroSeconds();
+	double totalMics = end - start;
+	cout << "Microseconds per 100 memory accesses: " << (100.0*totalMics)/sum << " ." << endl;
+	cout << "-----------------------------------------------------------------" << endl;
 }
 
 int main(int argc, char* argv[])
@@ -39,6 +51,8 @@ int main(int argc, char* argv[])
 	cout << "Execution started." << endl;
 	cout << "Allocating " << N << " bytes of memory..." << endl;
 	char* mem = initMemory();
-	doMemAccessMeasuremnts(mem, stride);
+	for(int stride = 1; stride <= 1024; stride*=2) {
+		doMemAccessMeasurements(mem, stride);
+	}
 	return 0;
 }
